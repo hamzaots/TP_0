09db5c32436168926dbed3f504b59c4e52bb1954
Hamza <hamzaots31@gmail.com>
2024-02-26 09:32:55 +0100

    push commit

--- src/test/java/FibonnacciTest.java
+++ src/test/java/FibonnacciTest.java
@@ -8,14 +8,17 @@ public class FibonnacciTest {
         int result=Fibonacci.fibonacci(0);
         assertEquals(Exception.class, result);
     }
+    @Test
     public void testnisnull(){
         int result=Fibonacci.fibonacci(0);
         assertEquals(0, result);
     }
+    @Test
     public void testnisequal1(){
         int result=Fibonacci.fibonacci(1);
         assertEquals(1, result);
     }
+    @Test
     public void elseall(){
         int result=Fibonacci.fibonacci(2);
         assertEquals(1, result);
